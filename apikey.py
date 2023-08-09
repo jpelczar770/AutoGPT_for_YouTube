f96c9c9b18ae62176274988773d6d665f8590366
Julia Pelczar <julia77044@gmail.com>
2023-08-09 19:27:39 +0200

    restored app.py

---
+++ apikey.py
@@ -0,0 +1,1 @@
+apikey = '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'
--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -18,16 +20,24 @@ title_template = PromptTemplate(
 
 )
 script_template = PromptTemplate(
-    input_variables = ['title'],
+    input_variables = ['title', 'wikipedia_research'],
     template = 'write me a youtube video script based on this title TITLE: {title}'
 )
 
+memory = ConversationBufferMemory(input_key = 'topic', memory_key='chat history')
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=memory)
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
 
+#
 
 if prompt:
-    response = sequential_chain.run(topic=prompt)
-    st.write(response)
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
+    with st.expander('Chat History'):
+        st.info(memory.buffer)
+
+
+        #testing
