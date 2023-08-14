7bbc636f424745e9037b58c841ee0a692370917b
jpelczar770 <127199593+jpelczar770@users.noreply.github.com>
2023-08-14 08:57:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ prompt = st.text_input("Hi, give me a topic of your next YouTube content!")
 title_template = PromptTemplate(
     input_variables=['topic'],
     template='write me a youtube video title about {topic}'
-
 )
+
 script_template = PromptTemplate(
     input_variables=['title', 'wikipedia_research'],
     template='write me a youtube video script based on this title TITLE: {title} while leveraging this wikipedia research: {wikipedia_research}'
